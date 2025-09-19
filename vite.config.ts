716199e529838c01c99175dd4a8bd2b34338ead6
Robert Will <matey.jack@gmail.com>
2025-09-19 20:20:57 +0200

    temporarily fix base path for GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  plugins: [preact()],
+    base: '/shunting-game/',
+    plugins: [preact()],
     build: {
         // because Github pages only takes it from here
         outDir: 'docs',
     },
-  test: {
-    environment: 'jsdom'
-  }
+    test: {
+        environment: 'jsdom'
+    }
 })
